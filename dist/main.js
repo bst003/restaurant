facf3dd90cacadf5987e42dfe1a67126b0940070
bst003 <bstanner33@yahoo.com>
2022-05-21 22:28:30 -0400

    Remove un-needed logs

--- dist/main.js
+++ dist/main.js
@@ -3,13 +3,9 @@ var __webpack_exports__ = {};
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-console.log('test');
 
-
-console.log('hello das world');
 
 
 
 /******/ })()
 ;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBOzs7QUFHQSIsInNvdXJjZXMiOlsid2VicGFjazovL3Jlc3RhdXJhbnQvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coJ3Rlc3QnKTtcblxuXG5jb25zb2xlLmxvZygnaGVsbG8gZGFzIHdvcmxkJyk7XG5cblxuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
