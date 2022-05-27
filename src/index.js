dc4e2a1bb45c5d5ba453acf392a2aa8e5bc78d86
bst003 <bstanner33@yahoo.com>
2022-05-27 11:36:51 -0400

    Remove notes from site

--- src/index.js
+++ src/index.js
@@ -1,27 +1,5 @@
 import './assets/scss/styles.scss';
 
-/*
-
-BEFORE DOING ANYTHING WRITE OUT YOUR PLAN
-
-
-General Setup
-    Start by creating headers and footers in their own modules
-    Each tab will have its own module
-        Factory to create tab modules?
-    Have a general site setup/creation function which will call the header/footer/tab creation functions
-
-
-Miscellaneous
-    tab switching event listeners will need to be added after page load
-        will first remove existing .module then add new one
-
-NEW NOTES
-    Potentially refactor all templates into one template file?
-    Should createWrap be with createHero and createModule when two are module specific?
-
-*/
-
 import { pageLoad } from "./modules/pageLoad";
 import { generalFunctions } from "./modules/general";
 import { homeFunctions } from "./modules/templates/home";
